4aa5a8fe173dc07e8cf7db99f64989e1a75a9ea3
Cathella <ncathie214@gmail.com>
2022-01-07 13:45:57 +0300

    Add react-router, Setup App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Details from './components/Details';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/main" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </Router>
   );
 }
 
