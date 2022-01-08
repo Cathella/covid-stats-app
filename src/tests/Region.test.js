788e0b096dbdd4d9e040b951e5c16fff1857ec4f
Cathella <ncathie214@gmail.com>
2022-01-09 02:35:12 +0300

    Remove double quotes

--- src/tests/Region.test.js
+++ src/tests/Region.test.js
@@ -6,16 +6,16 @@ import Region from '../components/Region';
 
 describe('Region card loads as it should', () => {
   const data = {
-    "date": "2022-01-07",
-    "name": "Madrid",
-    "today_confirmed": 1206297,
-    "today_deaths": 16381,
-    "today_new_confirmed": 21445,
-    "today_new_deaths": 3,
-    "today_new_open_cases": 21442,
-    "today_new_recovered": 78,
-    "today_open_cases": 1189916,
-    "today_recovered": 0,
+    date: '2022-01-07',
+    name: 'Madrid',
+    today_confirmed: 1206297,
+    today_deaths: 16381,
+    today_new_confirmed: 21445,
+    today_new_deaths: 3,
+    today_new_open_cases: 21442,
+    today_new_recovered: 78,
+    today_open_cases: 1189916,
+    today_recovered: 0,
   };
 
   it('Component has the number of total recovered cases', () => {
